e35620a6299e565e23b791179396596aeca87f7f
dreizehnutters <reach.fmk@gmail.com>
2025-05-20 22:49:54 +0200

    switched to XPath

--- nmap2csv.rb
+++ nmap2csv.rb
@@ -13,117 +13,54 @@ class NmapXMLParser
   end
 
   def parse(file_path)
-    File.open(file_path) do |file|
-      parser = Nokogiri::XML::SAX::Parser.new(NmapSAXHandler.new(@service_results, @host_results))
-      parser.parse(file)
-    end
-  end
+    doc = Nokogiri::XML(File.read(file_path))
 
-  class NmapSAXHandler < Nokogiri::XML::SAX::Document
-    def initialize(service_results, host_results)
-      super()
-      @current_host = nil
-      @current_port = nil
-      @current_service = nil
-      @service_results = service_results
-      @host_results = host_results
-    end
+    doc.xpath('//host').each do |host_node|
+      ip = host_node.at_xpath('address[@addrtype="ipv4"]/@addr')&.value
+      mac = host_node.at_xpath('address[@addrtype="mac"]/@addr')&.value
+      mac_vendor = host_node.at_xpath('address[@addrtype="mac"]/@vendor')&.value
+      hostnames = host_node.xpath('hostnames/hostname/@name').map(&:value)[0]
+      os_name = host_node.at_xpath('os/osmatch/@name')&.value
 
-    def start_element(name, attrs = [])
-      attrs_hash = Hash[attrs]
+      @host_results << {
+        'IP-Address' => ip,
+        'Hostname' => hostnames,
+        'Mac' => mac,
+        'Mac-Vendor' => mac_vendor, 
+        'OS' => os_name
+      }
 
-      if name == 'host'
-        @current_host = {
-          ip: nil,
-          hostnames: [],
-          mac: nil,
-          os: {
-            name: nil,
-            flavor: nil,
-            sp: nil
-          },
-          mac_vendor: nil,
-          ports: []
+      host_node.xpath('ports/port').each do |port_node|
+        portid = port_node['portid']
+        protocol = port_node['protocol']
+        state = port_node.at_xpath('state/@state')&.value
+
+        service_node = port_node.at_xpath('service')
+        next unless service_node
+
+        @service_results << {
+          'IP-Address' => ip,
+          'Hostname' => hostnames,
+          'Mac' => mac,
+          'Mac-Vendor' => mac_vendor,
+          'OS name' => os_name,
+          'Port' => portid,
+          'Protocol' => protocol,
+          'Service' => service_node['conf'].to_i >= 5 ? service_node['name'] : 'N/A',
+          'State' => state,
+          'Tunnel' => service_node['tunnel'],
+          'HTTP-Title' => port_node.at_xpath('script[@id="http-title"]/@output')&.value,
+          'Info' => [
+            service_node['product'],
+            service_node['version'],
+            service_node['extrainfo']
+          ].compact.reject(&:empty?).join(' ').strip()
         }
-      elsif name == 'address' && @current_host
-        if attrs_hash['addrtype'] == 'ipv4'
-          @current_host[:ip] = attrs_hash['addr']
-        elsif attrs_hash['addrtype'] == 'mac'
-          @current_host[:mac] = attrs_hash['addr']
-          @current_host[:mac_vendor] = attrs_hash['vendor']
-        end
-      elsif name == 'hostname' && @current_host
-        @current_host[:hostnames] << attrs_hash['name']
-      elsif name == 'osmatch' && @current_host
-        @current_host[:os][:name] = attrs_hash['name'] if attrs_hash['name']
-      elsif name == 'osclass' && @current_host
-        @current_host[:os][:sp] = attrs_hash['osgen']
-      elsif name == 'port' && @current_host
-        @current_port = {
-          portid: attrs_hash['portid'],
-          protocol: attrs_hash['protocol'],
-          state: nil,
-          service: nil
-        }
-        @current_host[:ports] << @current_port
-      elsif name == 'state' && @current_port
-        @current_port[:state] = attrs_hash['state']
-      elsif name == 'service' && @current_port
-        conf_value = attrs_hash['conf'] ? attrs_hash['conf'].to_i : 0
-        @current_service = {
-          name: attrs_hash['name'],
-          tunnel: attrs_hash['tunnel'],
-          product: attrs_hash['product'],
-          version: attrs_hash['version'],
-          extrainfo: attrs_hash['extrainfo'],
-          conf: conf_value,
-          http_title: nil
-        }
-        @current_port[:service] = @current_service
-      elsif name == 'script' && @current_service
-        if attrs_hash['id'] == 'http-title'
-          @current_service[:http_title] = attrs_hash['output'] if attrs_hash['output']
-        end
-      end
-    end
-
-    def end_element(name)
-      if name == 'host' && @current_host
-        @current_host[:hostnames] = @current_host[:hostnames].join(', ')
-
-        @host_results << {
-          address: @current_host[:ip],
-          mac: @current_host[:mac],
-          name: @current_host[:hostnames],
-          os_name: @current_host[:os][:name],
-          os_flavor: @current_host[:os][:flavor],
-          os_sp: @current_host[:os][:sp]
-        }
-
-        @current_host[:ports].each do |port|
-          next if port[:service] && port[:service][:conf] <= 5
-
-          @service_results << {
-            ip: @current_host[:ip],
-            hostnames: @current_host[:hostnames],
-            mac: @current_host[:mac],
-            mac_vendor: @current_host[:mac_vendor],
-            os_name: @current_host[:os][:name],
-            port: port[:portid],
-            tcp_udp: port[:protocol],
-            protocol: port[:service] ? port[:service][:name] : nil,
-            state: port[:state],
-            tunnel: port[:service] ? port[:service][:tunnel] : nil,
-            http_title: port[:service] ? port[:service][:http_title] : nil,
-            service_info: port[:service]
-          }
-        end
-
-        @current_host = nil
       end
     end
   end
 end
+
 
 class NmapCLI
   SERVICES_CSV_FILENAME = 'services.csv'
@@ -132,7 +69,7 @@ class NmapCLI
   def self.run
     options = {}
     OptionParser.new do |opts|
-      opts.banner = "Usage: nmap_cli_tool.rb [options]"
+      opts.banner = "Usage: nmap2csv.rb [options]"
 
       opts.on("-f", "--file PATH", "Path to an Nmap XML file or directory (required)") do |file|
         options[:file] = file
@@ -181,85 +118,30 @@ class NmapCLI
     FileUtils.mkdir_p(File.dirname(output_dir))
 
     puts "[*] Writing service infos to: #{services_csv_path}"
-    deduplicated_services = deduplicate_services(parser.service_results)
-    write_service_csv(deduplicated_services, services_csv_path)
+    write_csv(parser.service_results, services_csv_path)
 
     puts "[*] Writing host infos to: #{hosts_csv_path}"
-    deduplicated_hosts = deduplicate_hosts(parser.host_results)
-    write_hosts_csv(deduplicated_hosts, hosts_csv_path)
+    write_csv(parser.host_results, hosts_csv_path)
 
     puts "[*] CSV generation completed."
   end
-
-  def self.deduplicate_services(services)
-    services.uniq { |s| [s[:ip], s[:port], s[:tcp_udp], s[:protocol], s[:state], s[:tunnel]] }
-  end
-
-  def self.deduplicate_hosts(hosts)
-    hosts.uniq { |h| h[:address] }
-  end
-
-  def self.write_service_csv(results, output_file)
-    headers = [
-      'IP-address',
-      'Hostname',
-      'mac',
-      'mac-vendor',
-      'os_name',
-      'port',
-      'tcp/udp',
-      'protocol',
-      'state',
-      'tunnel',
-      'http-title',
-      'Info'
-    ]
-
-    CSV.open(output_file, 'w', col_sep: ';', quote_char: '"', write_headers: true, headers: headers) do |csv|
-      results.each do |result|
-        service_info = result[:service_info] || {}
-        info = [service_info[:product], service_info[:version], service_info[:extrainfo]].compact.join(' ').strip
-        csv << [
-          result[:ip],
-          result[:hostnames],
-          result[:mac],
-          result[:mac_vendor],
-          result[:os_name],
-          result[:port],
-          result[:tcp_udp],
-          result[:protocol], 
-          result[:state], 
-          result[:tunnel], 
-          result[:http_title],
-          info
-        ]
+  
+  def self.write_csv(results, output_file)
+    return if results.empty?
+    headers = results.first.keys
+    CSV.open(output_file,
+            'w',
+            col_sep: ';',
+            quote_char: '"',
+            force_quotes: true,
+            write_headers: true,
+            headers: headers) do |csv|
+      results.each do |row|
+        csv << headers.map { |h| row[h] }
       end
     end
   end
 
-  def self.write_hosts_csv(results, output_file)
-    headers = [
-      'address',
-      'mac',
-      'name',
-      'os_name',
-      'os_flavor',
-      'os_sp'
-    ]
-
-    CSV.open(output_file, 'w', col_sep: ';', quote_char: '"', write_headers: true, headers: headers) do |csv|
-      results.each do |result|
-        csv << [
-          result[:address],
-          result[:mac],
-          result[:name],
-          result[:os_name],
-          result[:os_flavor],
-          result[:os_sp]
-        ]
-      end
-    end
-  end
 end
 
 NmapCLI.run if __FILE__ == $PROGRAM_NAME
